bf72155a54166ab06c6dbe5338e7ac1d68c0e1bd
gemaquejr <gemaquejr@hotmail.com>
2022-09-23 18:12:37 -0300

    Adiciona a rota post

--- index.js
+++ index.js
@@ -6,15 +6,41 @@ require('dotenv/config');
 
 const { PORT } = process.env;
 
-const app = express()
+const app = express();
 
-app.get ('/users', async (req, res => {
-    try{
+app.use(express.json());
+
+app.get('/users', async(req, res => {
+    try {
         const users = await fs.read();
         return res.status(200).json(users);
     } catch (error) {
         console.log(error.message);
     }
 }));
+
+app.post('/users', async (req, res) => {
+    try {
+        const { name, state } = req.body;
+
+        const newUser = {
+            name,
+            city,
+            state,
+        };
+
+        const currentUser = await fs.read();
+
+        newUser.id = currentUser.length + 1;
+
+        const updateUser = { ...currentUser, newUser };
+
+        await fs.write(updateUser);
+
+        return res.status(201).json(newUser);
+    } catch (error) {
+        console.log(error.message);
+    };
+});
 
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}!`))
