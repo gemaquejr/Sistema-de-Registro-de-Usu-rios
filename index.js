cb7f95b2e4d1d1cc46d44c30003dd3cea0d351c1
gemaquejr <gemaquejr@hotmail.com>
2022-09-23 17:59:51 -0300

    Adiciona importacao de fs no index.js da raiz para criacao da rota get

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 
+const fs = require('./helpers');
+
 require('dotenv/config');
 
 const { PORT } = process.env;
 
 const app = express()
+
+app.get ('/users', async (req, res => {
+    try{
+        const users = await fs.read();
+        return res.status(200).json(users);
+    } catch (error) {
+        console.log(error.message);
+    }
+}));
 
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}!`))
